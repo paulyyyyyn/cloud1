f59ecd48dff128296de99e4cff4eb2f5258b16f1
Danthon Paulyn <paulyndanthon@gmail.com>
2025-04-01 16:54:29 +0200

    enable users routing

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 
 const indexRouter = require('./routes/index')
-//const usersRouter = require('./routes/users')
+const usersRouter = require('./routes/users')
 
 const app = express()
 
@@ -17,11 +17,10 @@ app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
-
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
-//app.use('/users', usersRouter)
+app.use('/users', usersRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
